8571348fa071a96493c56026e6c87edd26d15352
Karljiang118440 <jiang_chaoqing@126.com>
2019-07-30 11:06:00 +0800

    single number

---
+++ 剑指offer/Single Number.cpp
@@ -0,0 +1,14 @@
+ #if 0       
+        unordered_map<int,int> umap;
+        for(int i=0;i<n;i++)
+        {
+            umap[A[i]]++;
+        }
+        for (int i=0;i<n;i++)
+        {
+            if(umap[A[i]]==1)
+                return A[i];
+        }
+        
+    }
+ #endif
